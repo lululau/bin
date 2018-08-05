663ba56d81d79da5a9656bc2a0b73a71726a75b3
Liu Xiang <liuxiang@ktjr.com>
2018-08-05 15:49:19 +0800

    Revise

--- open_with_emacs.sh
+++ open_with_emacs.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-if echo "$1" | grep -q '\(\.rb$\)\|\(.rake$\)\|\(Gemfile\)' && [ -n "$2" ]
+if echo "$1" | grep -q '^[$~/]'; then
+  file=$1
+elif echo "$1" | grep -q "^[a-z]\+://"
 then
-    /usr/local/bin/emacsclient -n +"$2" "$1"
+  file=$1
 else
-    open "$1"
+  iterm2_pwd=$(cat /tmp/iterm2_pwd)
+  if [ -n "$iterm2_pwd" ]; then
+    file=$iterm2_pwd/$1
+  else
+    file=~$1
+  fi
+fi
+
+if echo "$file" | grep -q '\(\.rb$\)\|\(.rake$\)\|\(Gemfile\)' && [ -n "$2" ]
+then
+    /usr/local/bin/emacsclient -n +"$2" "$file"
+else
+    open "$file"
 fi

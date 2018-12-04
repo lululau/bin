c2741a2999dfa2f1ec25268bc0c2c167dcd854d5
Liu Xiang <liuxiang@ktjr.com>
2018-12-04 13:50:52 +0800

    Add scripts

--- mail-notifier.sh
+++ mail-notifier.sh
@@ -3,7 +3,14 @@
 export PATH=/usr/local/bin:$PATH
 
 if pgrep -q Emacs; then
+  if [ "$1" = inbox ]; then
     emacsclient -n --eval "(mu4e-update-mail-and-index t)"
+  # else
+  #   offlineimap
+  #   mv ~/Maildir/Archive/new/* ~/Maildir/Archive/cur/
+  #   mv ~/Maildir/Deleted\ Messages/new/* ~/Maildir/Deleted\ Messages/cur/
+  #   offlineimap
+  fi
 else
     terminal-notifier -title 'New Mail' -message 'There are some new mails' -sender org.gnu.emacs
 fi

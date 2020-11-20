12d2c4234f1caea285f061c3f933beec5aec162e
Liu Xiang <liuxiang@ktjr.com>
2020-11-20 22:20:47 +0800

    Link for brew-installed Python 3.8

--- update-pyenv-brew-installed-python.zsh
+++ update-pyenv-brew-installed-python.zsh
@@ -5,6 +5,10 @@ ln -nfs /usr/local/Cellar/python@2/2*(On[1]) ~/.pyenv/versions/
 ln -nfs /usr/local/Cellar/python@2/2*(On[1]) ~/.pyenv/versions/2
 ln -nfs /usr/local/Cellar/python@3.9/3*(On[1]) ~/.pyenv/versions/
 ln -nfs /usr/local/Cellar/python@3.9/3*(On[1]) ~/.pyenv/versions/3
+ln -nfs /usr/local/Cellar/python@3.8/3*(On[1]) ~/.pyenv/versions/3.8
 cd ~/.pyenv/versions/3/bin
+cp -Rf python3 python
+cp -Rf pip3 pip
+cd ~/.pyenv/versions/3.8/bin
 cp -Rf python3 python
 cp -Rf pip3 pip

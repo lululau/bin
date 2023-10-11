c297e8c4bd9639a3a1cb695ebcaa2f830ff4ceea
Liu Xiang <liuxiang@ktjr.com>
2023-10-11 10:45:27 +0800

    Add script to open files with Neovide

---
+++ open-with-neovide.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+##
+# This script is used to open a file with an existing Neovide instance or a new
+# one if none exists.
+
+if [ -z "$1" ]; then
+  echo "No file specified."
+  exit 1
+fi
+
+neovide_pid=$(ps -ef | grep Neovide | grep -v grep | awk '{print $2}')
+if [ -z "$neovide_pid" ]; then
+  open -a Neovide
+  sleep 1
+  neovide_pid=$(ps -ef | grep Neovide | grep -v grep | awk '{print $2}')
+fi
+
+if [ -z "$neovide_pid" ]; then
+  echo "Neovide is not running, and could not be started."
+  exit 1
+fi
+child_nvim_pid=$(ps -ef | grep $neovide_pid | grep -v 'grep\|Neovide' | awk '{print $2}')
+if [ -z "$child_nvim_pid" ]; then
+  echo "Neovide is running, but no child nvim process was found."
+  exit 1
+fi
+
+server_address=$(lsof -p $child_nvim_pid | grep "nvim.$child_nvim_pid" | awk '{print $NF}')
+if [ -z "$server_address" ]; then
+  echo "Neovide is running, but no server address was found."
+  exit 1
+fi
+
+full_paths=()
+for file in "$@"; do
+  full_paths+=("$(realpath "$file")")
+done
+
+open -a Neovide
+nvim --server "$server_address" --remote "${full_paths[@]}"

f6fe9365a3cab6c2900cda267b810524fa5d3869
Liu Xiang <liuxiang@ktjr.com>
2023-11-22 10:47:02 +0800

    Update

--- osx-env-sync.sh
+++ osx-env-sync.sh
@@ -1,3 +1,10 @@
 grep "^export" $HOME/.zshenv | while IFS=' =' read ignoreexport envvar ignorevalue; do
-  launchctl setenv ${envvar} ${!envvar}
+  launchctl setenv "${envvar}" "${!envvar}"
 done
+
+if [ -e /opt/homebrew/bin/brew ]; then
+  if launchctl getenv PATH | grep -v -q /opt/homebrew; then
+    launchctl setenv PATH "/opt/homebrew/bin:/opt/homebrew/sbin:$(launchctl getenv PATH)"
+  fi
+fi
+

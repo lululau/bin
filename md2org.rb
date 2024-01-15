4d6f5b5b37b4caac5ec2acabf8a14cdb926c06b8
Liu Xiang <liuxiang@ktjr.com>
2024-01-15 22:47:51 +0800

    Add md2org.rb

---
+++ md2org.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+if ARGV.length == 0
+  if STDIN.tty?
+    input = `/usr/bin/pbpaste`
+  else
+    input = STDIN.read
+  end
+
+  if STDOUT.tty?
+    output = :PBCOPY
+  else
+    output = STDOUT
+  end
+else
+  input = File.read(ARGV[0])
+  output = File.open(ARGV[1], 'w')
+end
+
+IO.popen("pandoc --filter=$HOME/.config/pandoc-org-filter.py --columns=120 -f markdown -t org", "r+") do |io|
+  io.write(input)
+  io.close_write
+  if output == :PBCOPY
+    IO.popen("/usr/bin/pbcopy", "w") { |f| f.write(io.read) }
+  else
+    output.write(io.read)
+  end
+end

f2fa53933c280ce81df242071ee26e75572e70f4
Liu Xiang <liuxiang921@gmail.com>
2026-04-25 11:43:22 +0800

    设置 iTerm2 中 yazi 启动时启用无装饰模式
    - 添加 BARE_YAZI_IN_TERM=1 环境变量以禁用 yazi 的窗口装饰
    - 保持原有 PATH 配置不变，确保工具链可用
    - 通过 exec 直接替换当前 shell 进程运行 yazi，提升启动效率

--- yazi-for-iterm2.sh
+++ yazi-for-iterm2.sh
@@ -2,4 +2,6 @@
 
 export PATH=/Users/liuxiang/bin:/Users/liuxiang/.local/bin:/Users/liuxiang/Library/Python/3.14/bin:/Users/liuxiang/Library/Python/2.7/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/Users/liuxiang/.pyenv/shims:/Users/liuxiang/.jenv/shims:/Users/liuxiang/.jenv/bin:/Users/liuxiang/.rvm/gems/ruby-4.0.0/bin:/Users/liuxiang/.rvm/gems/ruby-4.0.0@global/bin:/Users/liuxiang/.rvm/rubies/ruby-4.0.0/bin:/usr/local/bin:/System/Cryptexes/App/usr/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Users/liuxiang/.luarocks/bin:/Users/liuxiang/.swiftly/bin:/Applications/iTerm.app/Contents/Resources/utilities:/Applications/Obsidian.app/Contents/MacOS:/Users/liuxiang/.bun/bin:/Users/liuxiang/.cargo/bin:/usr/local/sbin:/Users/liuxiang/.fzf/bin:/Users/liuxiang/.rvm/bin:/Users/liuxiang/perl5/bin:/Users/liuxiang/.docker/bin:/Users/liuxiang/.antigravity/antigravity/bin:/Users/liuxiang/.agent-view/bin:/opt/homebrew/opt/go/libexec/bin:/Users/liuxiang/.go/bin:/Users/liuxiang/.orbstack/bin:
 
+export BARE_YAZI_IN_TERM=1
+
 exec /opt/homebrew/bin/yazi

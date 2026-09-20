d31b7f1851585517c1b8af260cdfd859a4814889
Liu Xiang <liuxiang921@gmail.com>
2026-09-20 15:02:02 +0800

    移除已废弃的 pyenv 版本管理逻辑，精简 Homebrew 更新脚本
    - 移除 brew-post-update 中冗余的 Python 版本符号链接和 bin 文件复制逻辑
    - 移除 update-all 中重复的 pyenv 版本自动配置代码
    - 移除独立脚本 update-pyenv-brew-installed-python.zsh，其功能已过时
    - 从 yazi-for-iterm2.sh 的 PATH 中移除不再需要的 ~/.pyenv/shims 路径

--- update-pyenv-brew-installed-python.zsh
+++ update-pyenv-brew-installed-python.zsh
@@ -7,18 +7,3 @@ if [ -z "$HOMEBREW_PREFIX" ]; then
     HOMEBREW_PREFIX="/usr/local"
   fi
 fi
-
-
-
-mkdir -p ~/.pyenv/versions
-ln -nfs $HOMEBREW_PREFIX/Cellar/python@2/2*(On[1]) ~/.pyenv/versions/
-ln -nfs $HOMEBREW_PREFIX/Cellar/python@2/2*(On[1]) ~/.pyenv/versions/2
-ln -nfs $HOMEBREW_PREFIX/Cellar/python@3.9/3*(On[1]) ~/.pyenv/versions/
-ln -nfs $HOMEBREW_PREFIX/Cellar/python@3.9/3*(On[1]) ~/.pyenv/versions/3
-ln -nfs $HOMEBREW_PREFIX/Cellar/python@3.8/3*(On[1]) ~/.pyenv/versions/3.8
-cd ~/.pyenv/versions/3/bin
-cp -Rf python3 python
-cp -Rf pip3 pip
-cd ~/.pyenv/versions/3.8/bin
-cp -Rf python3 python
-cp -Rf pip3 pip
